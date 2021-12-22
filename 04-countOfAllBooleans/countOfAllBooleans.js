86bbf86611e541db3c16182f44171f05cf95f617
Shua <Shua@MacBook-Air.fios-router.home>
2021-12-22 02:06:59 -0500

    04

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,13 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  count = 0;
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === true || arr[i] === false){
+      count++;
+    }
+  }
+
+  return count;
 }
 
 // Do not edit this line;
