60c8870ab7cf69ae08b7c6a24db47fe17be4769a
Shua <Shua@MacBook-Air.fios-router.home>
2021-12-22 14:36:50 -0500

    13

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,17 @@
 function isPalindrome(word) {
-  // Insert code here;
+  start = 0; 
+  end = word.length - 1;
+  
+  while(start < end){
+    if(word[start] == word[end]){
+      start++;
+      end--;
+    } else{
+      return false; 
+    }
+  }
+
+  return true; 
 }
 
 // Do not edit this line;
