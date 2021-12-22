57b53372d651e06736765cf6f6d0f41d42370cbe
Shua <Shua@MacBook-Air.fios-router.home>
2021-12-22 01:50:52 -0500

    01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,18 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  
+  newTitle = title.charAt(0).toUpperCase();
+  
+  for(let i = 1; i < title.length; i++){
+    if(title.charAt(i) == " "){
+      newTitle += " ";
+      newTitle += title.charAt(i + 1).toUpperCase();
+      i++;
+    } else{
+      newTitle += title.charAt(i);
+    }
+  }
+
+  return newTitle;
 }
 
 // Do not edit this line;
