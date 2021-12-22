e5b8a17af509eaffb226add7d331b95f9e154293
Shua <Shua@MacBook-Air.fios-router.home>
2021-12-22 13:27:54 -0500

    07

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,13 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+  count = 0; 
+  
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] == i){
+      count++;
+    }
+  }
+
+  return count; 
 }
 
 // Do not edit this line;
