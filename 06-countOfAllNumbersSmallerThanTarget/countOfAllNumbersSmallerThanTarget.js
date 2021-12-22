8d703301a46c44c777185de363913bb4e39ffab5
Shua <Shua@MacBook-Air.fios-router.home>
2021-12-22 13:22:32 -0500

    06

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,13 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+  count = 0;
+  
+  for(let i = 0; i < nums.length; i++){
+    if(nums[i] < target){
+      count++;
+    }
+  }
+
+  return count;
 }
 
 // Do not edit this line;
