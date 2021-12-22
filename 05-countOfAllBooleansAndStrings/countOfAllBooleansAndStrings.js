163df24a6845be945f56d8e83f287c76ea1adc31
Shua <Shua@MacBook-Air.fios-router.home>
2021-12-22 13:18:51 -0500

    05

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,13 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  count = 0;
+  
+  for(let i = 0; i < arr.length; i++){
+    if(typeof arr[i] == "string" || typeof arr[i] == "boolean"){
+      count++;
+    }
+  }
+
+  return count;
 }
 
 // Do not edit this line;
