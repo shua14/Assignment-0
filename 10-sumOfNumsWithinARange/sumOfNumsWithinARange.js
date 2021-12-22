777729f1e01be25afe4c51d85cede6cf869ca081
Shua <Shua@MacBook-Air.fios-router.home>
2021-12-22 14:05:36 -0500

    10

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,22 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+  count = 0;
+
+  flag = false; 
+  for(let i = 0; i < nums.length; i++){
+    if(!flag){
+      if(nums[i] == start){
+        count++; 
+        flag = true; 
+      }
+    } else{
+        if(nums[i] == end){
+          return ++count;
+        } else{
+          count++;
+        }
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
