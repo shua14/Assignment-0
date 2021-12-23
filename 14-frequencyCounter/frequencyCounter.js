e61e45334507aee7ec41f90fa027f9851e6d1f83
Shua <Shua@MacBook-Air.fios-router.home>
2021-12-22 22:12:21 -0500

    14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,15 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  let map = new Map();
+
+  for(let i = 0; i < word.length; i++){
+    if(map.has(word.charAt(i))){
+      map.set(word.charAt(i), map.get(word.charAt(i)) + 1);
+    } else{
+      map.set(word.charAt(i), 1);
+    }
+  }
+  
+  return Object.fromEntries(map);
 }
 
 // Do not edit this line;
