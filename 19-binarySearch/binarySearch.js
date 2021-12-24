349187159bf990841bfd7b31b46ca12943f4280f
Shua <Shua@MacBook-Air.fios-router.home>
2021-12-24 01:29:59 -0500

    19

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -4,10 +4,22 @@ class MySolution {
   }
 
   binarySearch(nums, target) {
-    // Insert code here;
+    let length = nums.length;
+    if (length === 0) {
+        return false;
+    } else {
+        let m = Math.floor(length / 2);
+        let y = nums[m];
+        if (target === y) {
+            return true;
+        } else if (target < y) {
+            return this.binarySearch(nums.slice(0, m), target);
+        } else {
+            return this.binarySearch(nums.slice(m + 1), target);
+        }
+    }
   }
 }
-
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
